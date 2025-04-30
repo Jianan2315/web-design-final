a65473fcc7598290b06442a5eb36f6717a6586a8
Jianan <ni.jian@northeastern.edu>
2025-04-29 22:25:11 -0400

    stop serving react

--- api/server.js
+++ api/server.js
@@ -6,14 +6,6 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3824;
 const HOST = process.env.HOST || '127.0.0.116';
 
-// Serve static files from the /public/dist directory
-app.use(express.static(path.join(__dirname, 'public', 'dist')));
-
-// Catch-all handler to serve index.html for React routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'dist', 'index.html'));
-});
-
 // Start the server
 app.listen(PORT, HOST, () => {
     console.log(`Server is running on http://${HOST}:${PORT}`);
